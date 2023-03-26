023e73d00a83a2e70d98c86bc8e3f360b3a73ba2
SreehariRajan <77159058+SreehariRajan@users.noreply.github.com>
2023-03-26 13:30:21 +0530

    url

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://muhammed-mizaj-automatic-spork-76pxrgwxrphwq6g-8000.preview.app.github.dev";
+export const BASE_URL = "https://muhammed-mizaj-congenial-xylophone-59w5pqpqpp93vpj4-8000.preview.app.github.dev";
